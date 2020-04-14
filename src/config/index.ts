40bf3ff876edf64afcb18b87060299220039783c
jhoncbernal <jhoncbernal@gmail.com>
2020-04-13 22:07:26 -0500

    [FIX]:enviroment change to prod

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,4 +1,4 @@
-export const BASEURL:string ="http://localhost:4000";// "https://vecinoo.herokuapp.com";
+export const BASEURL:string ="https://vecinoo.herokuapp.com";//"http://localhost:4000";// 
 export const APIVERSION:string ="/v1/api";// process.env.APIVERSION;
 export const MONGO_URI = process.env.MONGO_URI;
 export const APPLICATION_NAME = process.env.APPLICATION_NAME;
