1da0f3c351d0dd2ce91632c5f824f2a18c28a55f
jhoncbernal <jhoncbernal@gmail.com>
2020-05-31 22:02:15 -0500

    [ADD]:deep links

--- android/app/src/main/java/io/ionic/starter/MainActivity.java
+++ android/app/src/main/java/io/ionic/starter/MainActivity.java
@@ -1,5 +1,7 @@
 package io.ionic.vecinoo;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import com.getcapacitor.BridgeActivity;
@@ -10,12 +12,16 @@ import java.util.ArrayList;
 public class MainActivity extends BridgeActivity {
   @Override
   public void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
+      super.onCreate(savedInstanceState);
 
-    // Initializes the Bridge
-    this.init(savedInstanceState, new ArrayList<Class<? extends Plugin>>() {{
-      // Additional plugins you've installed go here
-      // Ex: add(TotallyAwesomePlugin.class);
-    }});
+      // Initializes the Bridge
+      this.init(savedInstanceState, new ArrayList<Class<? extends Plugin>>() {{
+          // Additional plugins you've installed go here
+          // Ex: add(TotallyAwesomePlugin.class);
+      }});
+      // ATTENTION: This was auto-generated to handle app links.
+      Intent appLinkIntent = getIntent();
+      String appLinkAction = appLinkIntent.getAction();
+      Uri appLinkData = appLinkIntent.getData();
   }
 }
