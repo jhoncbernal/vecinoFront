f1294b43a4beba998f498faa42282f5641b59e48
Jhon Casallas <jhoncbernal@gmail.com>
2022-03-12 18:11:33 -0500

    [MOD]:add provider service

--- src/services/provider.ts
+++ src/services/provider.ts
@@ -1,5 +1,30 @@
 import { api, paths } from ".";
+const urlPath = paths.ProviderContext;
 
-export const login = async ({ login }: any) => {
-  return await api.post(paths.LoginContext, login);
+export const getProvider = async ({ providerId }: any) => {
+  return await api.get(`${urlPath}/${providerId}`);
+};
+
+export const getAllProviders = async () => {
+  return await api.get(`${urlPath}`);
+};
+
+export const getProviderCities = async () => {
+  return await api.get(`${urlPath}/cities/1`);
+};
+
+export const getProviderByCity = async ({ cityId }: any) => {
+  return await api.get(`${urlPath}/names/${cityId}`);
+};
+
+export const addProvider = async ({ provider }: any) => {
+  return await api.post(`${urlPath}`, provider);
+};
+
+export const updateProvider = async ({ providerId, providerData }: any) => {
+  return await api.patch(`${urlPath}/${providerId}`, providerData);
+};
+
+export const deleteProvider = async ({ providerId }: any) => {
+  return await api.delete(`${urlPath}/${providerId}`);
 };
