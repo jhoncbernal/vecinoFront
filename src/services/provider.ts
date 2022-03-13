0bd178577f3c8e508c1ac9f64280ae1883541744
Jhon Casallas <jhoncbernal@gmail.com>
2022-03-12 20:09:43 -0500

    [FIX]:provider update

--- src/services/provider.ts
+++ src/services/provider.ts
@@ -21,8 +21,8 @@ export const addProvider = async ({ provider }: any) => {
   return await api.post(`${urlPath}`, provider);
 };
 
-export const updateProvider = async ({ providerId, providerData }: any) => {
-  return await api.patch(`${urlPath}/${providerId}`, providerData);
+export const updateProvider = async ({ providerId, provider }: any) => {
+  return await api.patch(`${urlPath}/${providerId}`, provider);
 };
 
 export const deleteProvider = async ({ providerId }: any) => {
